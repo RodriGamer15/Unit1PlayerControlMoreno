1be74c1dcf7de76e4e891a1d76b2b11e61b6e28b
Rodrigo Moreno <750694@student.dusd.net>
2024-03-01 15:22:28 -0800

    Finished the project
    I added the finishing touches to the game and added the bonus features, which were vehicles moving, a camera switcher, and pyramids of obstacles.

--- P8PlayerControlMoreno/Assets/Scripts/PlayerController.cs
+++ P8PlayerControlMoreno/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,38 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    // Private Variables
+    private float speed = 10.0f;
+    private float turnSpeed = 35.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
         // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
