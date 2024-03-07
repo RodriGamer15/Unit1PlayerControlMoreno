7f10f73b4e9f4aa5af4fe0cdf748182f95606675
Rodrigo Moreno <750694@student.dusd.net>
2024-03-07 14:37:18 -0800

    Finished the challenge
    I finished the plane challenge in my game and finished making the scripts

--- P8PlayerControlMoreno/Assets/Challenge 1/Scripts/FollowPlayerX.cs
+++ P8PlayerControlMoreno/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class FollowPlayerX : MonoBehaviour
 {
     public GameObject plane;
-    private Vector3 offset;
+    private Vector3 offset = new Vector3(30, 0, 10);
+    public Vector3 camRotation = new Vector3(0, -90, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-
+        offset = transform.position - plane.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = plane.transform.position + offset;
+        transform.rotation = Quaternion.Euler(camRotation);
     }
 }
